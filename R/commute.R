c0661bcfeca75600ce5411fbeb9046c7c9bb177a
andrew <arenn@design.upenn.edu>
2020-08-03 19:57:30 -0400

    Sampling points

--- R/commute.R
+++ R/commute.R
@@ -86,9 +86,10 @@ od_data <-
 
 cbgs_sf <- 
   projected %>%
+  st_transform(4326) %>%
   transmute(geo_code = GEOID) %>%
   st_centroid()
-
+  
 ##
 
 travel_network <- od2line(flow = od_data, zones = st_transform(cbgs_sf, 4326))
@@ -97,176 +98,106 @@ plot(travel_network, lwd = w)
 
 ##
 
-library(dodgr)
-
-routes <- line2route(travel_network, route_fun = route_dodgr)
-
-##
-
-routes <- tibble()
-
-coords <- 
-  cbgs %>% 
-  st_transform(4326) %>%
-  st_centroid() %>%
+cbgs_sf <-
+  cbgs_sf %>%
   st_coordinates() %>%
-  as_tibble() %>%
-  bind_cols(cbgs) %>%
-  st_as_sf() %>%
-  rename(geo_code = GEOID) %>%
-  select(geo_code, X, Y)
-
-for (i in 1:nrow(od_data)) {
-  
-  start  <- 
-    od_data %>%
-    slice(i) %>%
-    transmute(geo_code = geo_code1) %>%
-    left_join(coords) %>%
-    select(X, Y)
-    
-  finish <- 
-    od_data %>%
-    slice(i) %>%
-    transmute(geo_code = geo_code2) %>%
-    left_join(coords) %>%
-    select(X, Y)
-  
-  route <- route_dodgr(from = start, to = finish) %>% 
-    st_as_sf() %>%
-    as_tibble()
-  
-  routes <- bind_rows(routes, mutate(route, type = i))
-
-}  
-
-##
-
-roads <- read_sf("data/network/edges.shp")
-
-##
-
-graph <- sf_to_tidygraph(roads)
-
-plot(graph,
-     vertex.size = 0.5,
-     vertex.label = '',
-     alpha = 0.5)
-
-##
-
-network <- 
-  graph %>%
-  activate(edges) %>%
-  filter(from != to) %>%
-  mutate(length = st_length(geometry))
-
-##
-
-ggplot() +
-  geom_sf(data = network %>% activate(edges) %>% as_tibble() %>% st_as_sf() %>% st_transform(st_crs(louisville)) %>% st_intersection(louisville), size = 0.1) + 
-  geom_sf(data = network %>% activate(nodes) %>% as_tibble() %>% st_as_sf() %>% st_transform(st_crs(louisville)) %>% st_intersection(louisville), size = 0.1) +
-  theme_void()
-
-##
-
-network <- 
-  network %>%
-  activate(nodes) %>%
-  mutate(degree = centrality_degree()) %>%
-  mutate(betweenness = centrality_betweenness(weights = length)) %>%
-  activate(edges) %>%
-  mutate(betweenness = centrality_edge_betweenness(weights = length))
-
-##
-
-distances <- distances(graph = network, weights = network %>% activate(edges) %>% pull(length))
-
-##
-
-intersections <- 
-  network %>% 
-  activate(nodes) %>%
-  as_tibble() %>%
-  st_as_sf() %>%
-  st_transform(2205) %>%
-  st_coordinates() %>%
-  as_tibble()
-
-##
-
-library(RANN)
-
-##
-
-waypoints <- 
-  comb %>%
-  filter(w_bg %in% louisville$GEOID) %>%
-  filter(w_bg != h_bg) %>%
-  gather(location, GEOID, h_bg:w_bg) %>%
-  left_join(projected) %>%
-  mutate(jobs = S000) %>%
-  select(GEOID, id, location, X, Y, jobs)
-
-nn <- nn2(intersections, select(waypoints, X, Y), k = 1, searchtype = "radius", radius = 10000)
-
-length(nn$nn.idx[, 1])
-
-nearest <-
-  waypoints %>%
-  mutate(NODEID = nn$nn.idx[, 1])
-
-nearest <-
-  network %>% 
-  activate(nodes) %>%
-  as_tibble() %>%
-  right_join(nearest) %>%
-  select(NODEID, id, location, X, Y, jobs, degree, betweenness, geometry)
-
-##
-
-chunk <-  pivot_wider(nearest, id_cols = id, names_from = location, values_from = NODEID)
-
-##
-
-routes <- tbl_graph()
-
-##
-
-for (i in 1:nrow(chunk)) {
-  
-  from_node <- chunk[i, 2]
-  to_node <- chunk[i, 3]
-  
-  path <- 
-    shortest_paths(graph = network,
-                   from = from_node,
-                   to = to_node,
-                   output = 'both',
-                   weights = network %>% activate(edges) %>% pull(length))
-  
-  path_graph <- 
-    network %>%
-    subgraph.edges(eids = path$epath %>% unlist()) %>%
-    as_tbl_graph()
-  
-  path_graph <-
-    path_graph %>%
-    activate(edges) %>%
-    mutate(length = path_graph %>%
-             activate(edges) %>%
-             as_tibble() %>%
-             summarise(length = sum(length)) %>%
-             pull(length),
-           id = rep(chunk[i, 1], n()))
-  
-  routes <- bind_graphs(routes, path_graph)
-  
-}
-
-path_graph %>% 
-  activate(edges) %>% 
   as_tibble() %>% 
-  st_as_sf() %>% 
-  select(length) %>% 
-  plot()
+  bind_cols(cbgs_sf) %>% 
+  select(-geometry)
+
+od_data %>% 
+  mutate(id = paste(geo_code1, geo_code2, sep = " to ")) %>%
+  mutate(geo_code = geo_code1) %>%
+  left_join(cbgs_sf) %>%
+  rename(X_h = X, 
+         Y_h = Y) %>%
+  mutate(geo_code = geo_code2) %>%
+  left_join(cbgs_sf) %>%
+  rename(X_w = X, 
+         Y_w = Y) %>%
+  rename(geocode_h = geo_code1,
+         geocode_w = geo_code2) %>%
+  select(id, geocode_h, geocode_w, X_h, Y_h, X_w, Y_w, jobs) %>%
+  write_csv("chunk.csv")
+
+##
+
+travel_streets <- read_sf("data/imputed.shp")
+
+##
+
+cbgs_sf <- 
+  projected %>%
+  st_transform(4326) %>%
+  transmute(geo_code = GEOID)
+
+##
+
+o <- 
+  od_data %>% 
+  mutate(id = paste(geo_code1, geo_code2, sep = " to ")) %>%
+  mutate(geo_code = geo_code1) %>%
+  left_join(cbgs_sf) %>%
+  st_as_sf()
+
+d <- 
+  od_data %>% 
+  mutate(id = paste(geo_code1, geo_code2, sep = " to ")) %>%
+  mutate(geo_code = geo_code2) %>%
+  left_join(cbgs_sf) %>%
+  st_as_sf()
+
+origin_dots <- 
+  reduce(
+    map(1:nrow(od_data), 
+        ~st_sample(o$geometry[.x], size = round(o$jobs[.x] / 2), type = 'random') %>%
+          st_cast('POINT') %>%
+          st_coordinates() %>%
+          as_tibble() %>%
+          set_names(c("X_h", "Y_h")) %>%
+          mutate(id = o$id[.x])),
+    rbind)
+
+destin_dots <- 
+  reduce(
+    map(1:nrow(od_data), 
+        ~st_sample(d$geometry[.x], size = round(d$jobs[.x] / 2), type = 'random') %>%
+          st_cast('POINT') %>%
+          st_coordinates() %>%
+          as_tibble() %>%
+          set_names(c("X_w", "Y_w")) %>%
+          mutate(id = d$id[.x])),
+    rbind)
+
+nrow(destin_dots) == nrow(origin_dots)
+
+##
+
+origin_dots %>% 
+  st_as_sf(coords = c("X_h", "Y_h"), remove = FALSE) %>% 
+  select(id) %>% 
+  plot() 
+
+destin_dots %>% 
+  st_as_sf(coords = c("X_w", "Y_w"), remove = FALSE) %>% 
+  select(id) %>% 
+  plot() 
+
+dots <- 
+  bind_cols(origin_dots %>% 
+              mutate(id_o = id) %>%
+              select(-id),
+            destin_dots %>%
+              mutate(id_d = id) %>%
+              select(-id)) %>%
+  select(id_o, id_d, X_h, Y_h, X_w, Y_w)
+
+sum(dots$id_o == dots$id_d)
+
+dots %>%
+  mutate(id = id_o) %>%
+  select(-id_o, -id_d) %>%
+  select(id, everything()) %>%
+  write_csv("dots.csv")
+
+##s
